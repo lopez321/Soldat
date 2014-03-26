19889cdb3c90372be263632e18ccf8796d3918d3
ostojan <ostojan@gmail.com>
2014-03-26 19:31:28 +0100

    Start

---
+++ Soldat/Gra/main.cpp
@@ -0,0 +1,10 @@
+/*
+*	To bêdzie projekt samej gry.
+*	Myœlê, ¿e SFML bêdzie do tego najwygodniejszy, bo jest doœæ lekki i szybki.
+*/
+
+int main()
+{
+	return 0;
+}
+
---
+++ Soldat/Sieć/main.cpp
@@ -0,0 +1,9 @@
+/*
+*	To bêdzie projekt samego interfejsu sieciowego.
+*	Narazie jako zwyk³y konsolowy, ale póŸniej przerobimy na DLL-kê.
+*/
+
+int main()
+{
+	return 0;
+}
